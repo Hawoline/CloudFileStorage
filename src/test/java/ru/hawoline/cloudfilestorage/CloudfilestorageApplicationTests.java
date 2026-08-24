fb6f6f66a0b78da61baf97dd0bfc2263d80c7502
Hawoline <belikhawoline@gmail.com>
2026-08-24 16:03:36 +0800

    init

---
+++ src/test/java/ru/hawoline/cloudfilestorage/CloudfilestorageApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hawoline.cloudfilestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudfilestorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
